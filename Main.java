bef17ca2d6fa702ed713b7458b9851bf00950926
lp31827 <s31827@pjwstk.edu.pl>
2024-11-13 00:16:28 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        Client s31827_client =new Client("172.21.40.125",15000,"s31827");
-        s31827_client.run();
+        Client client =new Client(/*IPADRESSasString*/,/*PORT*/,/*USERNAME*/);
+        client.run();
     }
 }
